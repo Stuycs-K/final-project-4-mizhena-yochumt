93ff0908415057d538429be601d80ccaffa251a1
Theo Yochum <tyochum30@stuy.edu>
2023-05-25 15:57:42 -0400

    classwork

--- enigma.py
+++ enigma.py
@@ -46,12 +46,12 @@ def plugs(input, plugs):
     if not found:
       out += letter
   return out
-  
+
 def shift(let, offset):
   num = ord(let)
   shift = ord(offset) - ord('A')
-  if num + shift >= ord('Z'):
-    return chr(ord('A') + (shift % 26))
+  if num + shift > ord('Z'):
+    return chr(ord('A') + ((num + shift - ord('A')) % 26))
   return chr(num + shift)
 
 def shift_reverse(let, offset):
@@ -80,50 +80,65 @@ def rotor(input, r1, r2, r3, setting, ref):
     if not letter.isalpha():
       out += letter
       continue
-    if (setting[2] in turnover[r3]):
+    if (setting[2] in turnover[r3] and setting[1] in turnover[r2]):
       setting[1] = shift(setting[1], 'B')
-    
+
     if (setting[1] in turnover[r2]):
       setting[0] = shift(setting[0], 'B')
 
     setting[2] = shift(setting[2], 'B')
 
+    print(f'\n{setting[0]} {setting[1]} {setting[2]}')
+
     letter = shift(letter, setting[2])
     letter = rotate(r3, letter)
     letter = shift_reverse(letter, setting[2])
+    # print(f'Wheel 3: {letter}')
 
     letter = shift(letter, setting[1])
     letter = rotate(r2, letter)
     letter = shift_reverse(letter, setting[1])
+    # print(f'Wheel 2: {letter}')
 
     letter = shift(letter, setting[0])
     letter = rotate(r1, letter)
     letter = shift_reverse(letter, setting[0])
+    # print(f'Wheel 1: {letter}')
 
     letter = reflect(ref, letter)
+    # print(f'Reflector: {letter}')
 
     letter = shift(letter, setting[0])
     letter = rotate_reverse(r1, letter)
     letter = shift_reverse(letter, setting[0])
+    # print(f'Wheel 1: {letter}')
 
     letter = shift(letter, setting[1])
     letter = rotate_reverse(r2, letter)
     letter = shift_reverse(letter, setting[1])
+    # print(f'Wheel 2: {letter}')
 
     letter = shift(letter, setting[2])
     letter = rotate_reverse(r3, letter)
     letter = shift_reverse(letter, setting[2])
+    # print(f'Wheel 3: {letter}')
 
     out += letter
   return out
 
-setting = ['A', 'A', 'A']
+setting = ['D', 'D', 'S']
 
-message = "The Quick Brown fox jumped over the lazy dog".upper()
+message = "ABCDEFGHIJKLMNOP".upper()
 
 # message = plugs(message, plugboard)
 
-out = rotor(message, 1,2,3, setting, 2)
+# for i in range(0, 26):
+#     print(f'D reverse shifted by {i} = {shift_reverse("D", chr(ord("A") + i))}')
+#
+# for i in range(0, 26):
+#     print(f'D shifted by {i} = {shift("D", chr(ord("A") + i))}')
+
+out = rotor(message, 4,2,3, setting, 2)
 print(out)
 
 # message = plugs(message, plugboard)

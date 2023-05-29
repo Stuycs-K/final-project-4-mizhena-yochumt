ed928da9762a2855d30ddc000c64c7ef234ad2fb
Aiden Mizhen <amizhen30@stuy.edu>
2023-05-29 18:25:16 -0400

    ?

--- bombe.py
+++ bombe.py
@@ -105,47 +105,43 @@ print(''.join([chr(i+65) for i in known]), encoded)
 encoded = [ord(c) - 65 for c in encoded]
 
 
+pass_through(s0, s1, s2, r0, r1, r2, rf, t0, t1, known, encoded)
 
 
+for s0 in range(26):
+    for s1 in range(26):
+        for s2 in range(26):
+            s00, s11, s22 = s0, s1, s2
+            # Test each char
+            for p0 in range(26):
 
-# pass_through(s0, s1, s2, r0, r1, r2, rf, t0, t1, known, encoded)
+                mv_2 = False
+                for c in range(len(known)):
+                    given_in = known[c]
+                    given_out = encoded[c]
+                    s00 += 1
+                    s00 %= 26
+                    if s00 == t0:
+                        s11 += 1
+                        s11 %= 26
+                        if s11 == t1:
+                            mv_2 = True
+                    if mv_2:
+                        s11 += 1
+                        s22 += 1
+                        mv_2 = False
 
+                    e0 = (rotors[r0][(s00 + given_in) % 26] - s00) % 26
+                    e1 = (rotors[r1][(s11 + e0) % 26] - s11) % 26
+                    e2 = (rotors[r2][(s22 + e1) % 26] - s22) % 26
+                    r = reflectors[rf][e2 % 26]
+                    e3 = (rotors[r2].index((s22 + r) % 26) - s22) % 26
+                    e4 = (rotors[r1].index((s11 + e3) % 26) - s11) % 26
+                    e5 = (rotors[r0].index((s00 + e4) % 26) - s00) % 26
 
-# for s0 in range(26):
-#     for s1 in range(26):
-#         for s2 in range(26):
-#             s00, s11, s22 = s0, s1, s2
-#             # Test each char
-#             mv_2 = False
-#             for c in range(len(known)):
-#                 given_in = known[c]
-#                 given_out = encoded[c]
-#                 s00 += 1
-#                 s00 %= 26
-#                 if s00 == t0:
-#                     s11 += 1
-#                     s11 %= 26
-#                     if s11 == t1:
-#                         mv_2 = True
-#                 if mv_2:
-#                     s11 += 1
-#                     s22 += 1
-#                     mv_2 = False
-#
-#                 e0 = (rotors[r0][(s00 + given_in) % 26] - s00) % 26
-#                 e1 = (rotors[r1][(s11 + e0) % 26] - s11) % 26
-#                 e2 = (rotors[r2][(s22 + e1) % 26] - s22) % 26
-#                 # print(chr(e2+65+s2))
-#                 r = reflectors[rf][e2 % 26]
-#                 # print(r)
-#                 e3 = (rotors[r2].index((s22 + r) % 26) - s22) % 26
-#                 # print(chr(e2+65-s2))
-#                 e4 = (rotors[r1].index((s11 + e3) % 26) - s11) % 26
-#                 e5 = (rotors[r0].index((s00 + e4) % 26) - s00) % 26
-#
-#                 if given_out != e5:
-#                     # print(given_out, e5)
-#                     break
-#                 elif c == len(known)-1:
-#                     print(s0, s1, s2)
-#                     exit()
+                    if given_out != e5:
+                        # print(given_out, e5)
+                        break
+                    elif c == len(known)-1:
+                        print(s0, s1, s2)
+                        exit()
